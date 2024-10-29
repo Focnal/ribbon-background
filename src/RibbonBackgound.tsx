2ab82ee88ce38ffcaf9db1f3b7d9f42901bced0e
lhy <focnal@qq.com>
2024-10-29 23:13:19 +0800

    fixed: unnecessary rerenders

--- src/RibbonBackgound.tsx
+++ src/RibbonBackgound.tsx
@@ -7,98 +7,107 @@ interface AnimatedBackgroundProps {
   excludedSelectors?: string[];
 }
 
-const RibbonBackground: React.FC<AnimatedBackgroundProps> = ({
-  zIndex = -1,
-  alpha = 0.5,
-  size = 100,
-  excludedSelectors = [],
-}) => {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+const RibbonBackground: React.FC<AnimatedBackgroundProps> = React.memo(
+  ({ zIndex = -1, alpha = 0.5, size = 100, excludedSelectors = [] }) => {
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-  useEffect(() => {
-    if (!canvasRef.current) return;
+    useEffect(() => {
+      if (!canvasRef.current) return;
 
-    const canvas = canvasRef.current;
-    const g2d = canvas.getContext("2d");
-    const pr = window.devicePixelRatio || 1;
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    const f = size;
-    let q: { x: number; y: number }[], t: number;
-    let r = 0;
-    const pi = Math.PI * 2;
-    const cos = Math.cos;
-    const random = Math.random;
+      const canvas = canvasRef.current;
+      const g2d = canvas.getContext("2d");
+      const pr = window.devicePixelRatio || 1;
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      const f = size;
+      let q: { x: number; y: number }[], t: number;
+      let r = 0;
+      const pi = Math.PI * 2;
+      const cos = Math.cos;
+      const random = Math.random;
 
-    if (!g2d) {
-      console.error("2D context not available");
-      return;
-    }
-
-    canvas.width = width * pr;
-    canvas.height = height * pr;
-    g2d.scale(pr, pr);
-    g2d.globalAlpha = alpha;
-    canvas.style.cssText = `opacity: ${alpha}; position: fixed; top: 0; left: 0; z-index: ${zIndex}; width: 100%; height: 100%; pointer-events: none;`;
-
-    function redraw() {
-      if (g2d) {
-        g2d.clearRect(0, 0, width, height);
-        q = [
-          { x: 0, y: height * 0.7 + f },
-          { x: 0, y: height * 0.7 - f },
-        ];
-        while (q[1].x < width + f) draw(q[0], q[1]);
+      if (!g2d) {
+        console.error("2D context not available");
+        return;
       }
-    }
 
-    function draw(i: { x: number; y: number }, j: { x: number; y: number }) {
-      if (g2d) {
-        g2d.beginPath();
-        g2d.moveTo(i.x, i.y);
-        g2d.lineTo(j.x, j.y);
-        const k = j.x + (random() * 2 - 0.25) * f;
-        const n = line(j.y);
-        g2d.lineTo(k, n);
-        g2d.closePath();
-        r -= pi / -50;
-        g2d.fillStyle = `#${(
-          ((cos(r) * 127 + 128) << 16) |
-          ((cos(r + pi / 3) * 127 + 128) << 8) |
-          (cos(r + (pi / 3) * 2) * 127 + 128)
-        ).toString(16)}`;
-        g2d.fill();
-        q[0] = q[1];
-        q[1] = { x: k, y: n };
+      canvas.width = width * pr;
+      canvas.height = height * pr;
+      g2d.scale(pr, pr);
+      g2d.globalAlpha = alpha;
+      canvas.style.cssText = `opacity: ${alpha}; position: fixed; top: 0; left: 0; z-index: ${zIndex}; width: 100%; height: 100%; pointer-events: none;`;
+
+      function redraw() {
+        if (g2d) {
+          g2d.clearRect(0, 0, width, height);
+          q = [
+            { x: 0, y: height * 0.7 + f },
+            { x: 0, y: height * 0.7 - f },
+          ];
+          while (q[1].x < width + f) draw(q[0], q[1]);
+        }
       }
-    }
-    function line(p: number): number {
-      t = p + (random() * 2 - 1.1) * f;
-      return t > height || t < 0 ? line(p) : t;
-    }
 
-    function handleRedraw(event: MouseEvent | TouchEvent) {
-      const target = event.target as HTMLElement;
-      const shouldExclude = excludedSelectors.some((selector) =>
-        target.closest(selector)
-      );
-      if (!shouldExclude) {
-        redraw();
+      function draw(i: { x: number; y: number }, j: { x: number; y: number }) {
+        if (g2d) {
+          g2d.beginPath();
+          g2d.moveTo(i.x, i.y);
+          g2d.lineTo(j.x, j.y);
+          const k = j.x + (random() * 2 - 0.25) * f;
+          const n = line(j.y);
+          g2d.lineTo(k, n);
+          g2d.closePath();
+          r -= pi / -50;
+          g2d.fillStyle = `#${(
+            ((cos(r) * 127 + 128) << 16) |
+            ((cos(r + pi / 3) * 127 + 128) << 8) |
+            (cos(r + (pi / 3) * 2) * 127 + 128)
+          ).toString(16)}`;
+          g2d.fill();
+          q[0] = q[1];
+          q[1] = { x: k, y: n };
+        }
       }
-    }
+      function line(p: number): number {
+        t = p + (random() * 2 - 1.1) * f;
+        return t > height || t < 0 ? line(p) : t;
+      }
 
-    document.addEventListener("click", handleRedraw);
-    document.addEventListener("touchstart", handleRedraw);
+      function handleRedraw(event: MouseEvent | TouchEvent) {
+        const target = event.target as HTMLElement;
+        const shouldExclude = excludedSelectors.some((selector) =>
+          target.closest(selector)
+        );
+        console.log(shouldExclude);
+        if (!shouldExclude) {
+          redraw();
+        }
+      }
 
-    redraw(); // Initial draw
+      document.addEventListener("click", handleRedraw);
+      document.addEventListener("touchstart", handleRedraw);
 
-    return () => {
-      document.removeEventListener("click", handleRedraw);
-      document.removeEventListener("touchstart", handleRedraw);
-    };
-  }, [alpha, size, zIndex, excludedSelectors]);
+      redraw(); // Initial draw
 
-  return <canvas ref={canvasRef} />;
-};
+      return () => {
+        document.removeEventListener("click", handleRedraw);
+        document.removeEventListener("touchstart", handleRedraw);
+      };
+    }, [alpha, size, zIndex, excludedSelectors]);
+
+    return <canvas ref={canvasRef} />;
+  },
+  (prevProps, nextProps) => {
+    // 进行自定义比较，决定是否重新渲染
+    return (
+      prevProps.alpha === nextProps.alpha &&
+      prevProps.size === nextProps.size &&
+      prevProps.zIndex === nextProps.zIndex &&
+      // 比较 excludedSelectors 的内容
+      prevProps.excludedSelectors?.join() ===
+        nextProps.excludedSelectors?.join()
+    );
+  }
+);
 
 export default RibbonBackground;
